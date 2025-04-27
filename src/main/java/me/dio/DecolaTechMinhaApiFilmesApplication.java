9b6bbebcc60626a6f381e8499710c100832a3723
Gabriel Galacci <gabrielcssoad@hotmail.com>
2025-04-27 00:12:21 -0300

    first commit with basic structure, json structure and README with classes diagram

---
+++ src/main/java/me/dio/DecolaTechMinhaApiFilmesApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecolaTechMinhaApiFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecolaTechMinhaApiFilmesApplication.class, args);
+	}
+
+}
